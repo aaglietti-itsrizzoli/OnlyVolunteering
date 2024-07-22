0b57593f219ad7f1c85cd8ab55e71f1dad7b6cd1
Immortalis202 <lmalegori@gmail.com>
2024-07-22 14:48:45 +0000

    added

--- javademo/src/main/java/com/mycompany/app/App.java
+++ javademo/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        int i = 0;
         System.out.println( "Hello World!" );
+        if(i ==0){
+            System.out.println("Ciao");
+        }
     }
 }
